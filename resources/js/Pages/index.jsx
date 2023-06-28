86dbcbd25e094ace4c0262233fcbfff05dc02d6d
sofyanegi <sofyanegil@gmail.com>
2023-06-28 07:49:00 +0700

    config inertia

---
+++ resources/js/Pages/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "@inertiajs/react";
+
+export default function index() {
+    return (
+        <>
+            <h1>Hello World</h1>
+        </>
+    );
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createInertiaApp, router } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
+
+createInertiaApp({
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.jsx", { eager: true });
+        return pages[`./Pages/${name}.jsx`];
+    },
+    setup({ el, App, props }) {
+        createRoot(el).render(<App {...props} />);
+    },
+});
+
+router.on("navigate", () => {
+    document.getElementById("app").removeAttribute("data-page");
+});
