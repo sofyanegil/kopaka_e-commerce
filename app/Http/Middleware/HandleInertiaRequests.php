9bab7e5fcb3a1af4743286cd1ed582cd837c62c7
Sofyan Egi Lesmana <sofyanegil@gmail.com>
2023-07-05 22:10:30 +0700

    config permissions

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,11 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            //
+            //user authenticated
+            'auth' => [
+                'user' => $request->user() ? $request->user() : null,
+                'permissions' => $request->user() ? $request->user()->getPermissionArray() : []
+            ]
         ]);
     }
 }
---
+++ resources/js/Utils/Permissions.jsx
@@ -0,0 +1,16 @@
+import { usePage } from "@inertiajs/react";
+
+export default function hasAnyPermission(permissions) {
+    // destruct auth from props
+    const { auth } = usePage().props;
+
+    // get all permissions from props
+    let allPermissions = auth.permissions;
+
+    let hasPermission = false;
+
+    permissions.forEach(function (item) {
+        if (allPermissions[item]) hasPermission = true;
+    });
+    return hasPermission;
+}
